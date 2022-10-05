288549d90e00c50b8433f9380e903d45e2d979f6
ykerit <ykerforit@gmail.com>
2022-10-06 01:06:50 +0800

    remove resovled lsp cap

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -56,22 +56,6 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 	border = "rounded",
 })
 
-local function lsp_highlight_document(client)
-	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
-		vim.api.nvim_exec(
-			[[
-      augroup lsp_document_highlight
-      autocmd! * <buffer>
-      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-      ]],
-			false
-		)
-	end
-end
-
 local function lsp_keymaps(bufnr)
 	local opts = {
 		noremap = true,
@@ -94,11 +78,7 @@ require("mason").setup()
 require("lsp_signature").setup({})
 
 local on_attach = function(client, bufnr)
-	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
-	end
 	lsp_keymaps(bufnr)
-	lsp_highlight_document(client)
 	require("lsp_signature").on_attach()
 end
 
